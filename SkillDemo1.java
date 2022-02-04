c0da3200c75831c2b9b9779887d96e1803804871
Pranav Reddy <pranav.n.reddy@gmail.com>
2022-02-03 17:22:57 -0800

    added exception for 0^0

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,5 +1,8 @@
 public class SkillDemo1 {
     public static int exponent(int base, int power) {
+        if(base == 0 && power == 0) {
+            throw new IllegalArgumentException();
+        }
         int result = 1;
         for(int i = 0; i < power; i++) {
             result = result * base;
